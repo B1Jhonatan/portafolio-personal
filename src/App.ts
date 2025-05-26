5c5d49c47c3e6ae2548f0b3fa6668671d03f94f1
Jhonatan Jaimes <jejo.arch@gmail.com>
2025-05-26 12:04:19 -0500

    Creando paginas

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,32 @@
 const app = document.querySelector<HTMLDivElement>("#app");
+const url = window.location.href;
+
+const root: string = "http://localhost:5173/";
+const proyecto: string = "http://localhost:5173/proyecto-front";
+
+let fetchUrl: string;
+
+function asignarUrl(): string {
+  if (url === root) {
+    fetchUrl = "/src/pages/portafolio/Portafolio.html";
+  } else if (url === proyecto) {
+    fetchUrl = "/src/pages/proyecto/Proyecto-front.html";
+  }
+  return fetchUrl;
+}
 
 if (app) {
-  fetch("/src/pages/portafolio/Portafolio.html")
-    .then((response) => {
+  fetch(asignarUrl())
+    .then((response: any) => {
       if (!response.ok) {
-        throw new Error("No se pudo cargar el header");
+        throw new Error("No se pudo cargar el HTML");
       }
       return response.text();
     })
-    .then((html) => {
+    .then((html: any) => {
       app.innerHTML = html;
     })
-    .catch((error) => {
-      console.error("Error al cargar el header:", error);
+    .catch((error: any) => {
+      console.error("Error al cargar el HTML:", error);
     });
 }
