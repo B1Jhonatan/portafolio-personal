335fecad6974a41e8399bcdff00a2b2b87f33e82
Jhonatan Jaimes <jejo.arch@gmail.com>
2025-06-30 13:07:41 -0500

    Configurando path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/", // Si tu app está en la raíz (https://tudominio.com/)
+  // O usa './' si tienes problemas con rutas relativas:
+  // base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
