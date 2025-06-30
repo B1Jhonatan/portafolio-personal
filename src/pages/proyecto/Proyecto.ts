168946cb3197b89e406086292d10dd40ca980bb0
Jhonatan Jaimes <jejo.arch@gmail.com>
2025-06-30 14:09:48 -0500

    Nuevos cambios

---
+++ src/pages/proyecto/Proyecto.ts
@@ -0,0 +1,19 @@
+import "./Portafolio.css";
+
+const portafolioPage = document.querySelector<HTMLDivElement>("#portafolio");
+
+if (portafolioPage) {
+  fetch("/components-static/portafolio-static/Portafolio.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("No se pudo cargar el header");
+      }
+      return response.text();
+    })
+    .then((html) => {
+      portafolioPage.innerHTML = html;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el header:", error);
+    });
+}
