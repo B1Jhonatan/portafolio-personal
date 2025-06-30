802361880df494793cf335806ce1552174445f28
Jhonatan Jaimes <jejo.arch@gmail.com>
2025-06-30 14:59:52 -0500

    Portafolio final

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
-import path from "path";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/", // Si tu app está en la raíz (https://tudominio.com/)
-  // O usa './' si tienes problemas con rutas relativas:
-  // base: './',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  base: "/", // IMPORTANTE
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
   },
 });
